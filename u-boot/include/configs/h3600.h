48b22f9272e43cece0b596adcf7895018a1d1356
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2014-02-02 22:18:40 +0100

    updated h3600 config

--- u-boot/include/configs/h3600.h
+++ u-boot/include/configs/h3600.h
@@ -23,7 +23,7 @@
 /*
  * Size of malloc() pool
  */
-#define CONFIG_SYS_MALLOC_LEN		(CONFIG_ENV_SIZE + 128*1024)
+#define CONFIG_SYS_MALLOC_LEN		(CONFIG_ENV_SIZE + 128 * 1024)
 
 /*
  * select serial console configuration
@@ -36,16 +36,22 @@
 /*
  * Command line configuration.
  */
-#include <config_cmd_default.h>
 #define CONFIG_CMD_FLASH
-#undef CONFIG_CMD_NET
-#undef CONFIG_CMD_NFS
-#undef CONFIG_CMD_FPGA
-#undef CONFIG_CMD_MISC
-#undef CONFIG_CMD_SETGETDCR
-#undef CONFIG_CMD_XIMG
+#define CONFIG_CMD_CONSOLE
+#define CONFIG_CMD_ECHO
+#define CONFIG_CMD_EDITENV
+#define CONFIG_CMD_LOADB
+#define CONFIG_CMD_RUN
+#define CONFIG_CMD_SAVEENV
+#define CONFIG_CMD_JFFS2
+#define CONFIG_CMD_MEMORY
 
-#define CONFIG_BOOTDELAY	50
+#undef CONFIG_BOOTM_NETBSD
+#undef CONFIG_BOOTM_PLAN9
+#undef CONFIG_BOOTM_RTEMS
+#undef CONFIG_BOOTM_VXWORKS
+
+#define CONFIG_BOOTDELAY	5
 #define CONFIG_BOOTARGS		"root=/dev/hda1 console=ttySA0,115200n8 console=tty1"
 #define CONFIG_BOOTCOMMAND	"run boot_kernel"
 #define CONFIG_SYS_AUTOLOAD	"n"	/* No autoload */
@@ -57,7 +63,7 @@
 #define CONFIG_SYS_LONGHELP	/* undef to save memory */
 #define CONFIG_SYS_PROMPT		"HP iPAQ 3600# "
 #define CONFIG_SYS_CBSIZE		256	/* console buffsize */
-#define CONFIG_SYS_PBSIZE		(256+sizeof(CONFIG_SYS_PROMPT)+16)
+#define CONFIG_SYS_PBSIZE		(256 + sizeof(CONFIG_SYS_PROMPT) + 16)
 #define CONFIG_SYS_MAXARGS		16	/* max number of command args */
 #define CONFIG_SYS_BARGSIZE		256	/* Boot Argument Buffer Size */
 #define CONFIG_SYS_MEMTEST_START	CONFIG_SYS_SDRAM_BASE	/* Start addr for test */
@@ -68,17 +74,13 @@
 
 #define CONFIG_SYS_FLASH_CFI		1
 #define CONFIG_FLASH_CFI_DRIVER		1
-#define CONFIG_FLASH_CFI_WIDTH		FLASH_CFI_32BIT
+#define CONFIG_SYS_FLASH_CFI_WIDTH	FLASH_CFI_32BIT
 #define CONFIG_SYS_FLASH_BASE		0x00000000
 #define CONFIG_SYS_FLASH_ERASE_TOUT	(0x1000)
 #define CONFIG_SYS_FLASH_WRITE_TOUT	(0x1000)
-#define CONFIG_SYS_FLASH_INCREMENT	0x02000000
-#define PHYS_FLASH_1			0x00000000	/* starts at 0x0 */
-#define PHYS_FLASH_SIZE			0x01000000	/* 16MB */
-#define PHYS_FLASH_SECT_SIZE		0x00040000	/* 256KB Sectors */
 #define CONFIG_SYS_MAX_FLASH_BANKS	1
-#define CONFIG_SYS_MAX_FLASH_SECT	260
-#define CONFIG_SYS_FLASH_BANKS_LIST	{ PHYS_FLASH_1 }
+#define CONFIG_SYS_MAX_FLASH_SECT	64
+#define CONFIG_SYS_FLASH_BANKS_LIST	{ CONFIG_SYS_FLASH_BASE }
 #define CONFIG_SYS_FLASH_EMPTY_INFO	1
 #define CONFIG_SYS_MONITOR_LEN		0x00040000
 #define CONFIG_SYS_MONITOR_BASE		0x00000000
@@ -104,7 +106,7 @@
 #define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_SDRAM_BASE + 0x1000 - \
 					GENERATED_GBL_DATA_SIZE)
 
-#define PHYS_SDRAM_1			0xC0000000	/* SDRAM Bank #1 */
+#define PHYS_SDRAM_1			0xc0000000	/* SDRAM Bank #1 */
 #define PHYS_SDRAM_1_SIZE		0x02000000	/* 32 MB */
 
 #define CONFIG_CMD_MTDPARTS
