132a48556edfd69404f4e2ab8e521bd323fe0ea9
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2014-02-02 20:33:13 +0100

    no need erase blocks

--- u-boot/setup.sh
+++ u-boot/setup.sh
@@ -16,7 +16,6 @@ make CROSS_COMPILE=arm-linux-gnueabi- ${1}_config && {
 		echo "detect" >> cmdsflash
 		echo "poke 0x49000000 1" >> cmdsflash
 		echo "detectflash 0" >> cmdsflash
-		echo "eraseflash 0 2" >> cmdsflash
 		echo "flashmem 0 u-boot.bin" >> cmdsflash
 		echo "reset" >> cmdsflash
 		echo "quit" >> cmdsflash
