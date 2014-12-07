def1c08ce6a85573d1ac1576516dd2a3567d0a76
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2014-12-07 07:57:35 +0100

    fixed boot from cf

--- u-boot/include/configs/h3600.h
+++ u-boot/include/configs/h3600.h
@@ -64,10 +64,11 @@
 #undef CONFIG_BOOTM_RTEMS
 #undef CONFIG_BOOTM_VXWORKS
 
-#define CONFIG_BOOTDELAY	5
-#define CONFIG_BOOTARGS		"root=/dev/mtdblock0 console=tty1 kgdbwait kgdboc=ttySA0,115200n8"
-/*#define CONFIG_BOOTARGS		"root=/dev/mtdblock0 console=ttySA0,115200n8 console=tty1"*/
-#define CONFIG_BOOTCOMMAND	"run copy_kernel;run boot_kernel"
+#define CONFIG_BOOTDELAY	1
+/*#define CONFIG_BOOTARGS	"root=/dev/mtdblock0 console=ttySA0,115200n8 console=tty1"*/
+/*#define CONFIG_BOOTCOMMAND	"run boot_kernel"*/
+#define CONFIG_BOOTARGS		"root=/dev/mtdblock0 console=tty1 loglevel=7 console=ttySA0,115200n8 kgdbwait kgdboc=ttySA0,115200"
+#define CONFIG_BOOTCOMMAND	"run copy_kernel;bootm"
 #define CONFIG_SYS_AUTOLOAD	"n"	/* No autoload */
 #define CONFIG_SYS_LOAD_ADDR	CONFIG_SYS_SDRAM_BASE
 
