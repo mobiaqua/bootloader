77fa0aab6167452a2bd3205e8911e3515b5123f6
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2016-05-16 07:04:45 +0200

    generate cmdsreset aswell

--- u-boot/setup.sh
+++ u-boot/setup.sh
@@ -10,27 +10,31 @@ fi
 
 function addcmd() {
 	echo "$1" >> cmdsflash
+	if [ "$2" != "n" ]; then
+		echo "$1" >> cmdsreset
+	fi
 }
 
 make CROSS_COMPILE=arm-linux-gnueabi- ${1}_config && {
 	rm -f cmdsflash
+	rm -f cmdsreset
 
 	case $1 in
 	h3600)
 		addcmd "cable ${MA_JTAG_ADAPTER}"
 		addcmd "detect"
-		addcmd "poke 0x49000000 1" # unlock flash for writting
+		addcmd "poke 0x49000000 1" n # unlock flash for writting
 		addcmd "detectflash 0"
-		addcmd "flashmem 0 u-boot.bin noverify"
+		addcmd "flashmem 0 u-boot.bin noverify" n
 		addcmd "reset"
 		addcmd "quit"
 		;;
 	h3800)
 		addcmd "cable ${MA_JTAG_ADAPTER}"
 		addcmd "detect"
-		addcmd "poke 0x49001f00 1" # unlock flash for writting
+		addcmd "poke 0x49001f00 1" n # unlock flash for writting
 		addcmd "detectflash 0"
-		addcmd "flashmem 0 u-boot.bin noverify"
+		addcmd "flashmem 0 u-boot.bin noverify" n
 		addcmd "reset"
 		addcmd "quit"
 		;;
